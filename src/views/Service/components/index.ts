b97fe5ea73ca4d2f4348d3bf78fdde198eac9322
MkDev11 <94194147+MkDev11@users.noreply.github.com>
2022-03-10 10:16:37 -0500

    1.0

--- src/views/Service/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as About } from './About';
-export { default as Advantages } from './Advantages';
-export { default as Integrations } from './Integrations';
-export { default as Team } from './Team';
-export { default as VideoSection } from './VideoSection';
-export { default as Faq } from './Faq';
